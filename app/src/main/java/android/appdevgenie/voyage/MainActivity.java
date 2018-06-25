6fc2517fdcebf149aa19bac506e63edda3d90667
appdevgenie <appdevgenie@gmail.com>
2018-06-25 10:03:31 +0200

    add firebase authentication and default firebase auth ui

--- app/src/main/java/android/appdevgenie/voyage/MainActivity.java
+++ app/src/main/java/android/appdevgenie/voyage/MainActivity.java
@@ -1,13 +1,102 @@
 package android.appdevgenie.voyage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN = 1;
+
+    private String username;
+    private Context context;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        initAuthStateListener();
+    }
+
+    private void initAuthStateListener() {
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                if (firebaseUser != null) {
+
+                    onSignedInInitialized(firebaseUser.getDisplayName());
+
+                } else {
+
+                    username = "";
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            SIGN_IN);
+                }
+            }
+        };
+    }
+
+    private void onSignedInInitialized(String displayName) {
+
+        username = displayName;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(context, context.getString(R.string.signed_in), Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED){
+                Toast.makeText(context, context.getString(R.string.sign_in_cancelled), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
